44d66bde7d3cc6f215f2e2205c6384370d2967e2
Seanna Musgrave <seannamusgrave@gmail.com>
2014-03-30 13:54:36 -0700

    Minor fix to target controller

--- Assets/Scripts/PlayerTargetController.js
+++ Assets/Scripts/PlayerTargetController.js
@@ -1,24 +1,28 @@
 ﻿function OnCollisionStay (collision: Collision) {
     if (collision.gameObject.name == "EnemySword") {
         if (madeImpact(collision)) {
-	        var EnemySword = GameObject.Find("EnemySword");
-	        var player = GameObject.Find("Player");
-	        var upperBody = player.transform.FindChild("UpperBody");
-	        var bloodSpout = upperBody.FindChild("PlayerBloodSpout").gameObject;
-	        
-	        bloodSpout.transform.position = EnemySword.transform.Find("Tip").position;
-	        bloodSpout.transform.rotation = EnemySword.transform.Find("Tip").rotation;
-	        bloodSpout.transform.rotation.z += 180;
-	        bloodSpout.particleSystem.Play();
-	        
-	        player.rigidbody.isKinematic = false;
-	        player.rigidbody.useGravity = true;
-	        
-	        GameObject.Destroy(GameObject.Find("LevelText"));	        	        
-	        GameObject.Find("ResetText").guiText.text = 'Press "R" to retry';
-	        player.GetComponent("PlayerController").wounded = true;
+            die();
 	     }
 	}
+}
+
+function die() {
+	var EnemySword = GameObject.Find("EnemySword");
+	var player = GameObject.Find("Player");
+	var upperBody = player.transform.FindChild("UpperBody");
+	var bloodSpout = upperBody.FindChild("PlayerBloodSpout").gameObject;
+
+	bloodSpout.transform.position = EnemySword.transform.Find("Tip").position;
+	bloodSpout.transform.rotation = EnemySword.transform.Find("Tip").rotation;
+	bloodSpout.transform.rotation.z += 180;
+	bloodSpout.particleSystem.Play();
+
+	player.rigidbody.isKinematic = false;
+	player.rigidbody.useGravity = true;
+
+	GameObject.Destroy(GameObject.Find("LevelText"));	        	        
+	GameObject.Find("ResetText").guiText.text = 'Press "R" to retry';
+	player.GetComponent("PlayerController").wounded = true;
 }
 
 function madeImpact(collision) {
