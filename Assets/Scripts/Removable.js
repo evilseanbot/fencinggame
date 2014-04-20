68c921809d25e7c3372941847f34713b285312ec
Seanna Musgrave <seannamusgrave@gmail.com>
2014-04-19 20:53:19 -0700

    ADDED HATS!!!

---
+++ Assets/Scripts/Removable.js
@@ -0,0 +1,8 @@
+﻿function OnCollisionStay (collision: Collision) {
+    if (collision.gameObject.name == "Sword") {
+        if (collision.gameObject.GetComponent("SwordController").swordUp) {
+            //rigidbody.constraints = RigidbodyConstraints.None;
+            rigidbody.isKinematic = false;
+	    }
+    }
+}
